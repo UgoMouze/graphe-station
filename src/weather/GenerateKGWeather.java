44501a8ec0db9d90a81a1594eaba94fd272ccaa9
Ugo MOUZE <ugo.mouze@etu.emse.fr>
2022-04-08 12:01:23 +0200

    Exemple du prof qui fonctionne
    implémentation du code du prof

--- src/weather/GenerateKGWeather.java
+++ src/weather/GenerateKGWeather.java
@@ -5,25 +5,24 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.io.IOException;
 
 public class GenerateKGWeather extends GenerateKG{
 
-    public GenerateKGWeather(int station, String date){
-        super();
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-
-
-    }
-
     public static void main(String[] args) {
-        Document doc = Jsoup.parse("https://www.meteociel.fr/temps-reel/obs_villes.php?affint=1&code2=7475&jour2=1&mois2=3&annee2=2022");
-        Elements elts = doc.selectXpath("/html/body/table[1]/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/center[2]/table[2]/tbody/tr");
-        for (Element elt : elts) {
-            // on selectionne l'heure
-            String time= elt.selectXpath("/tr/td[1]").first().text();
-            System.out.println(time);
+        try {
+            String url = "https://www.meteociel.fr/temps-reel/obs_villes.php?affint=1&code2=7475&jour2=1&mois2=3&annee2=2022";
+            Document doc = Jsoup.connect(url).get();
+            Elements elts = doc.selectXpath("/html/body/table[1]/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/center[2]/table[2]/tbody/tr");
+
+            for (Element elt : elts) {
+                // on selectionne l'heure
+                String time = elt.selectXpath("tr/td[1]").first().text();
+                System.out.println(time);
+            }
+        }
+        catch (IOException e){
+
         }
     }
 }
