340664c2c17b5bbd7c3ca62831cca0f447a8df79
Ugo MOUZE <ugo.mouze@etu.emse.fr>
2022-04-08 11:45:53 +0200

    First extraction
    implémentation du code du prof

--- src/weather/GenerateKG.java
+++ src/weather/GenerateKG.java
@@ -14,16 +14,17 @@ import java.util.Map;
 
 public class GenerateKG {
 
-    /*
+    /**
      * A "Model" in Jena is what represents a graph, but is mutable: new nodes and arcs can be added; nodes or arcs can be removed; etc.
      */
     protected Model kg;
-    /*
+
+    /**
      * This is to define a "base URI" for the identifiers of nodes and relations.
      */
     private String base;
 
-    /*
+    /**
      * Initialises an empty Jena Model (graph with no node nor arcs) and a few prefixes.
      */
     public GenerateKG() {
@@ -34,7 +35,7 @@ public class GenerateKG {
         kg.setNsPrefix("xsd", XSD.NS);
     }
 
-    /*
+    /**
      * Sets the base used to generate triples from Turtle bits that have relative IRIs.
      * The base must be a valid IRI.
      */
@@ -46,7 +47,7 @@ public class GenerateKG {
         return check;
     }
 
-    /*
+    /**
      * Sets a new namespace prefix.
      * The namespace must be a valid IRI.
      */
@@ -58,7 +59,7 @@ public class GenerateKG {
         return check;
     }
 
-    /*
+    /**
      * Serialises the prefix declarations in Turtle for all prefixes defined in the Jena Model
      */
     String prefixesInTurtle() {
@@ -69,7 +70,7 @@ public class GenerateKG {
         return ttlPrefixes;
     }
 
-    /*
+    /**
      * Add triples written in a String as Turtle code. Does not require declaring the prefixes if they are already in the NsPrefixes of the model
      */
     public void addTriples(String ttl) {
@@ -78,21 +79,21 @@ public class GenerateKG {
         kg.read(is,base,"Turtle");
     }
 
-    /*
+    /**
      * Serialises the knowledge graph to Turtle and display it on the console.
      */
     public void serialise() {
         kg.write(System.out,"Turtle");
     }
 
-    /*
+    /**
      * Serialises the knowledge graph to Turtle into an output stream given as parameter (e.g., a FileOutputStream)
      */
     public void serialise(OutputStream os) {
         kg.write(os,"Turtle");
     }
 
-    /*
+    /**
      * Main execution method
      */
     public static void main(String[] args) {
