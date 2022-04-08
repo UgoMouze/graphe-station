60acc1bc69198a58fa87a65bd125dfb47aab724a
Ugo MOUZE <ugo.mouze@etu.emse.fr>
2022-04-08 10:31:44 +0200

    Dossier
    Réorganisation des fichiers

--- src/weather/GenerateKG.java
+++ src/weather/GenerateKG.java
@@ -14,17 +14,16 @@ import java.util.Map;
 
 public class GenerateKG {
 
-    /**
+    /*
      * A "Model" in Jena is what represents a graph, but is mutable: new nodes and arcs can be added; nodes or arcs can be removed; etc.
      */
     protected Model kg;
-
-    /**
+    /*
      * This is to define a "base URI" for the identifiers of nodes and relations.
      */
     private String base;
 
-    /**
+    /*
      * Initialises an empty Jena Model (graph with no node nor arcs) and a few prefixes.
      */
     public GenerateKG() {
@@ -35,7 +34,7 @@ public class GenerateKG {
         kg.setNsPrefix("xsd", XSD.NS);
     }
 
-    /**
+    /*
      * Sets the base used to generate triples from Turtle bits that have relative IRIs.
      * The base must be a valid IRI.
      */
@@ -47,7 +46,7 @@ public class GenerateKG {
         return check;
     }
 
-    /**
+    /*
      * Sets a new namespace prefix.
      * The namespace must be a valid IRI.
      */
@@ -59,7 +58,7 @@ public class GenerateKG {
         return check;
     }
 
-    /**
+    /*
      * Serialises the prefix declarations in Turtle for all prefixes defined in the Jena Model
      */
     String prefixesInTurtle() {
@@ -70,7 +69,7 @@ public class GenerateKG {
         return ttlPrefixes;
     }
 
-    /**
+    /*
      * Add triples written in a String as Turtle code. Does not require declaring the prefixes if they are already in the NsPrefixes of the model
      */
     public void addTriples(String ttl) {
@@ -79,19 +78,36 @@ public class GenerateKG {
         kg.read(is,base,"Turtle");
     }
 
-    /**
+    /*
      * Serialises the knowledge graph to Turtle and display it on the console.
      */
     public void serialise() {
         kg.write(System.out,"Turtle");
     }
 
-    /**
+    /*
      * Serialises the knowledge graph to Turtle into an output stream given as parameter (e.g., a FileOutputStream)
      */
     public void serialise(OutputStream os) {
         kg.write(os,"Turtle");
     }
 
+    /*
+     * Main execution method
+     */
+    public static void main(String[] args) {
+        // Create a new KG generator
+        GenerateKG gen = new GenerateKG();
+        // Set the base URI to a custom value
+        gen.setBase("http://mydomain.org/weatherkg/");
+        // Define an additional prefix for the ontology namespace
+        gen.setNsPrefix("onto", "http://mydomain.org/ontology/");
+        // Add a single triple
+        gen.addTriples("<> a onto:Entity .");
+        // Add a bunch of triples at once
+        gen.addTriples("<> rdfs:label \"Test entity\"@en;"
+                + "  rdfs:commnt \"This is a test\"@en .");
+        gen.serialise();
 
+    }
 }
