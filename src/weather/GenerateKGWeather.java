d0b2af09608552e57d2c64a69a083d0a58c58dd7
Ugo Mouze <mouze44@gmail.com>
2022-05-01 17:29:34 +0200

    entire stationmeteo
    le contenu d'un page est entrée avec l'ontologie définie

--- src/weather/GenerateKGWeather.java
+++ src/weather/GenerateKGWeather.java
@@ -10,18 +10,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class GenerateKGWeather extends GenerateKG {
+public class GenerateKGWeather {
     /**
      * Main execution method
      */
 
     public static void main(String[] args) {
-        boolean jour2 = false;
-        boolean mois2 = false;
-        String jour;
-        Integer mois;
-        String ville;
-        String name = "src/turtlefiles/test.ttl";
+        String name = "src/turtlefiles/stationmeteo.ttl";
         // Create a new KG generator
         GenerateKG gen = new GenerateKG();
         // Set the base URI to a custom value
@@ -29,19 +24,18 @@ public class GenerateKGWeather extends GenerateKG {
         // Define an additional prefix for the ontology namespace
         gen.setNsPrefix("onto", "http://mydomain.org/ontology/");
 
-        try {
-            gen.serialise(new FileOutputStream(name));
-        } catch (FileNotFoundException ex) {
-            File file = new File(name);
-        }
 
         try {
-            String url = "https://www.meteociel.fr/temps-reel/obs_villes.php?affint=0&code2=7475&jour2=1&mois2=3&annee2=2021";
+            boolean jour2;
+            boolean mois2;
+            String jour;
+            Integer mois;
+            String ville;
+            String url = "https://www.meteociel.fr/temps-reel/obs_villes.php?affint=1&code2=7475&jour2=20&mois2=3&annee2=2021";
             Document doc = Jsoup.connect(url).get();
             Elements elts = doc.selectXpath("/html/body/table[1]/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/center[2]/table/tbody/tr");
             Elements loc = doc.selectXpath("/html/body/table[1]/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td/h1/center");
             ville = loc.get(0).text().substring(loc.get(0).text().indexOf("pour")+5, loc.get(0).text().indexOf(" ("));
-            System.out.println(ville);
 
             jour2 = url.substring(url.indexOf("jour2")+7, url.indexOf("jour2")+8) == "&";
             if (!jour2){
@@ -62,7 +56,7 @@ public class GenerateKGWeather extends GenerateKG {
             String annee = url.substring(url.indexOf("annee2")+7,url.indexOf("annee2")+11);
 
             gen.addTriples(
-                    "<"+ville+"> a onto:City ;"
+                    "<"+ville+"> a onto:Ville ;"
                             +" onto:Departement "+loc.get(0).text().substring(loc.get(0).text().indexOf("(")+1, loc.get(0).text().indexOf(")"))+" ."
             );
             gen.addTriples(
@@ -79,45 +73,83 @@ public class GenerateKGWeather extends GenerateKG {
             gen.addTriples(
                     "<Rapports" + Mois[mois] + annee + ">" +
                             " a onto:Month ;"
-                            +"onto:quand <Rapports" + annee + "> ;"
+                            +"onto:Year <Rapports" + annee + "> ;"
                             + " xsd:gYearMonth \""+annee + "-" + m + "\" .");
 
             gen.addTriples(
                     "<Rapports" + jour + Mois[mois] + annee + ">" +
                             " a onto:Day ;"
-                            +"onto:quand <Rapports" + Mois[mois] + annee + "> ;"
+                            +"onto:Month <Rapports" + Mois[mois] + annee + "> ;"
                             + " xsd:date \""+annee + "-" + m + "-"+jour +"\" .");
+            // On enlève les éléments inutilisables
+            for(int i = 0; i<3; i++){
+                elts.remove(0);
+            }
+            boolean changementHeure;
+            float temperature=0;
+            float precipitation=0;
+            int n=0;
+            String hour="";
             for (Element elt : elts) {
+
                 // on selectionne l'heure
                 String time = elt.selectXpath("tr/td[1]").first().text().replaceAll("\\s", "");
-                String temp = elt.selectXpath("tr/td[5]").first().text().replaceAll("\\s", "");
-                System.out.println(temp);
+                Float temp = Float.valueOf(elt.selectXpath("tr/td[5]").first().text().replaceAll("\\s", "").substring(0,elt.selectXpath("tr/td[5]").first().text().replaceAll("\\s", "").indexOf("°C"))).floatValue();
+                String precip = elt.selectXpath("tr/td[12]").first().text().replaceAll("\\s", "");
+                // initialisation
+                if(hour.compareTo("")==0){
+                    hour = time.substring(0, time.indexOf("h")+1);
+                }
+                changementHeure = time.substring(0, time.indexOf("h")+1).compareTo(hour)!=0;
 
+                if(changementHeure){
+                    temperature = temperature/n;
+                    gen.addTriples(
+                            "<TemperatureMoyenneHoraire"+hour+jour+Mois[mois]+annee+ville+"> " +
+                                    "a onto:TemperatureMoyenneHoraire ; " +
+                                    "onto:valeur \""+temperature+"\" ; " +
+                                    "xsd:unit \"Celsius\" ."
+                    );
+                    gen.addTriples(
+                            "<PrecipitationHoraire"+hour+jour+Mois[mois]+annee+ville+"> " +
+                                "a onto:PrecipitationHoraire ;" +
+                                "onto:valeur \""+precipitation+"mm\" ."
+                    );
 
-                gen.addTriples(
-                        "<Rapports" +time.substring(0, time.indexOf("h")+1)+ jour + Mois[mois] + annee + ">" +
-                                " a onto:Hour ;"
-                                +"onto:quand <Rapports" + jour + Mois[mois] + annee + "> ;"
-                                + " xsd:datetime \""+annee + "-" + m + "-"+jour +"\" .");
+                    gen.addTriples(
+                            "<Rapports" +hour+ jour + Mois[mois] + annee + ">" +
+                                    " a onto:Hour ;"
+                                    +"onto:Day <Rapports" + jour + Mois[mois] + annee + "> ;"
+                                    + " xsd:datetime \""+annee + "-" + m + "-"+jour +"T"+hour+"\" .");
 
-                gen.addTriples(
-                        "<MesureHoraire"+time.substring(0, time.indexOf("h")+1)+ jour + Mois[mois] + annee + ville+">" +
-                                "a onto:MesureHoraire ; " +
-                                "onto:quand Rapports" + time.substring(0, time.indexOf("h")+1)+ jour + Mois[mois] + annee +"; " +
-                                "onto:ou " + ville+" ; " +
-                                "onto:temperature AZE ; " +
-                                "onto:precipitation AZE ."
-                );
+                    gen.addTriples(
+                            "<MesureHoraire"+hour+ jour + Mois[mois] + annee + ville+">" +
+                                    "a onto:MesureHoraire ; " +
+                                    "onto:Hour <Rapports" + hour+ jour + Mois[mois] + annee +"> ; " +
+                                    "onto:Ville <" + ville+"> ; " +
+                                    "onto:TemperatureMoyenneHoraire <TemperatureMoyenneHoraire"+hour+jour+Mois[mois]+annee+ville+"> ; "+
+                                    "onto:PrecipitationHoraire <PrecipitationHoraire"+hour+jour+Mois[mois]+annee+ville+"> ."
+                    );
+
+                    hour = time.substring(0, time.indexOf("h")+1);
+                    n=0;
+                    temperature=0;
+                    precipitation=0;
+                }
+                else{
+                    temperature+=temp;
+                    if(precip.indexOf("aucune")!=0){
+                        precipitation+=Float.valueOf(precip.substring(0, precip.indexOf("mm")));
+                    }
+                    n++;
+                }
             }
-
             gen.serialise();
-
-
-
-
-
-
-
+            try {
+                gen.serialise(new FileOutputStream(name));
+            } catch (FileNotFoundException ex) {
+                new File(name);
+            }
         } catch (IOException e) {
         }
     }
